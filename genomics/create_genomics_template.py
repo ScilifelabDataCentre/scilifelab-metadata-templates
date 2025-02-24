f5e52c97e078c9c84c8ca3c6e349680559a4d4ce
Sonja Mathias <sonja.mathias@scilifelab.uu.se>
2025-02-24 13:33:06 +0100

    Add json template files (#24)
    * Reformat organisational metadata files to be consistent with ENA_target_metadata_fields.yml
    
    * Output genomics template as json
    
    * first suggestion for a yaml file with internal info like version
    
    * Refactor I/O code
    
    * Base json template file also off yaml file for orga metadata
    
    * Update description
    
    * Fix tsvs with correct orga metadata
    
    * Split json template into single and paired reads
    
    * Add template name and version to organisational metadata
    
    * Clean up code and rename file
    
    * Add info about json template files to genomics readme
    
    * Refactor code to review comments
    
    * Spell out cv as controlled_vocabulary
    
    ---------
    
    Co-authored-by: Rickard Hammarén <rickard.hammaren@scilifelab.uu.se>
    Co-authored-by: Adrien Coulier <adrien.coulier@medsci.uu.se>

--- genomics/create_genomics_template.py
+++ genomics/create_genomics_template.py
@@ -1,48 +1,96 @@
+"""
+Run create_genomics_template.py to create a SciLifeLab internal template 
+for the technical metadata of genomics data.
+
+It will read the organisational metadata fields from the file 
+'organisational_metadata_fields.yml' and the description and version fields 
+for the genomics template from the file 'genomics_template_wrapper.yaml'.
+"""
+
 import csv
 import yaml
+import json
 
-def get_fields_from_tsv(file_path):
-    with open(file_path, mode='r') as file:
-        reader = csv.reader(file, delimiter='\t')
-        fields = next(reader)
+def get_fields_from_yaml(file_path, label):
+    with open(file_path, mode='r') as f:
+        data = yaml.safe_load(f)
+        fields = data.get(label, {}).get('fields', [])
     return fields
 
-def get_fields_from_yaml(file_path):
-    with open(file_path, mode='r') as file:
-        data = yaml.safe_load(file)
-        fields = list(data.keys())
+def get_fields_from_json(file_path, label):
+    with open(file_path, mode='r') as f:
+        data = json.load(f)
+        fields = data.get(label, {}).get('fields', [])
     return fields
+
+def collect_fields(output_file_path):
+
+    orga_file_path = '../organisational_metadata_fields.yml'
+    orga_fields = get_fields_from_yaml(orga_file_path, 'organisational_metadata')
+
+    # get relevant json fields prefilled with CV terms fetched from ENA
+    json_file_path_ENA_fields = 'ENA_experiment_metadata_fields/ENA_experiment_metadata_fields.json'
+    experiment_fields = get_fields_from_json(json_file_path_ENA_fields, 'experiment')
+    run_fields = get_fields_from_json(json_file_path_ENA_fields, 'run')
+ 
+    all_fields_single_read = (
+        experiment_fields[:10] + experiment_fields[11:] # leave out insert_size
+        + run_fields[2:5] # use single file fields, leave out alias and experiment_alias fields
+        + orga_fields
+    )
+    all_fields_paired_reads = (
+        experiment_fields 
+        + [ run_fields[2] ] + run_fields[5:] # use forward and reverse file fields
+        + orga_fields
+    )
+
+    return all_fields_single_read, all_fields_paired_reads
+
+def write_fields_to_json(output_file_path, all_fields_single_read, all_fields_paired_reads):
+    
+    # get the metadata wrapper for the genomics template
+    internal_metadata_file_path = 'genomics_template_wrapper.yaml'
+    with open(internal_metadata_file_path, mode='r') as f:
+        internal_metadata = yaml.safe_load(f)
+   
+    # add the fields for single read and save to file
+    internal_metadata['genomics_template']['fields'] = all_fields_single_read
+    with open(output_file_path+"_single_read.json", mode='w') as f:
+        json.dump(internal_metadata, f, indent=4)
+
+    # add the fields for paired reads and save to file 
+    internal_metadata['genomics_template']['fields'] = all_fields_paired_reads
+    with open(output_file_path+"_paired_reads.json", mode='w') as f:
+        json.dump(internal_metadata, f, indent=4)
+    
+    print(f"Genomics template written to {output_file_path}_single_read.json and {output_file_path}_paired_reads.json")
+
+def write_field_names_to_tsv(output_file_path, all_fields_single_read, all_fields_paired_reads):
+
+    with open(output_file_path+"_single_read.tsv", mode='w', newline='') as f:
+        writer = csv.writer(f, delimiter='\t')
+        writer.writerow([field['name'] for field in all_fields_single_read])
+
+    with open(output_file_path+"_paired_reads.tsv", mode='w', newline='') as f:
+        writer = csv.writer(f, delimiter='\t')
+        writer.writerow([field['name'] for field in all_fields_paired_reads])
+
+    print(f"Genomics template field names written to {output_file_path}_single_read.tsv and {output_file_path}_paired_reads.tsv")
+
 
 if __name__ == "__main__":
-    # get ENA experiment metadata fields
-    ena_exp_file_path = 'ENA_experiment_metadata_fields/experiment.tsv'
-    ena_exp_fields = get_fields_from_tsv(ena_exp_file_path)
-
-    # get ENA run metadata fields
-    ena_run_file_path = 'ENA_experiment_metadata_fields/run.tsv'
-    ena_run_fields = get_fields_from_tsv(ena_run_file_path)
-
-    # for single read use single file fields, leave out alias and experiment_alias fields
-    single_read_fields = ena_run_fields[2:5]
-    # for paired reads use forward and reverse file fields
-    paired_reads_fields = [ena_run_fields[2]] + ena_run_fields[5:]
-
-    # get organisational metadata fields
-    orga_file_path = '../organisational_metadata_fields.yml'
-    orga_fields = get_fields_from_yaml(orga_file_path)
-
-    # leave out insert_size for single reads
-    all_fields_single_read = ena_exp_fields[:10] + ena_exp_fields[11:] + single_read_fields + orga_fields
-    all_fields_paired_reads = ena_exp_fields + paired_reads_fields + orga_fields
-
-    # write to individual tsv files
-    output_file_path = 'genomics_technical_metadata'
     
-    with open(output_file_path+"_single_read.tsv", mode='w', newline='') as file:
-        writer = csv.writer(file, delimiter='\t')
-        writer.writerow(all_fields_single_read)
+    output_file_path = 'genomics_technical_metadata'
 
-    with open(output_file_path+"_paired_reads.tsv", mode='w', newline='') as file:
-        writer = csv.writer(file, delimiter='\t')
-        writer.writerow(all_fields_paired_reads)
-    print(f"Genomics template fields written to {output_file_path}_single_read.tsv and {output_file_path}_paired_reads.tsv")
+    all_fields_single_read, all_fields_paired_reads = collect_fields(output_file_path)
+    
+    write_fields_to_json(output_file_path, all_fields_single_read, all_fields_paired_reads)
+
+    write_field_names_to_tsv(output_file_path, all_fields_single_read, all_fields_paired_reads)
+
+    
+
+
+
+
+ 
