11b4dfb35213b54f08543e40035bc69ad048e829
Sonja Mathias <sonja.mathias@scilifelab.uu.se>
2025-04-03 12:48:58 +0200

    Fix json schema

--- genomics/scripts/generate_json_schema.py
+++ genomics/scripts/generate_json_schema.py
@@ -1,58 +1,42 @@
 import json
 
-def generate_json_schema(json_data):
+def generate_json_schema(json_data, title="Genomics Template Schema"):
+
+    required = []
+    d = {}
+
+    for el in json_data["fields"]:
+        if isinstance(el, dict):
+            if el["field_type"] in ["TEXT_FIELD", "TEXT_AREA_FIELD"]:
+                d[el["name"]] = {
+                    "type": "string",
+                    "description": el["description"]
+                }
+            elif el["field_type"] == "TEXT_CHOICE_FIELD":
+                d[el["name"]] = {
+                    "type": "string",
+                    "description": el["description"],
+                    "enum": el["controlled_vocabulary"]                            
+                }
+            elif el["field_type"] == "DATE_FIELD":
+                d[el["name"]] = {
+                    "type": "string",
+                    "description": el["description"],
+                    "format": "date"
+                }
+            if el["cardinality"] == "mandatory":
+                required.append(el["name"])
+
     schema = {
         "$schema": "http://json-schema.org/draft-07/schema#",
         "type": "object",
-        "properties": {}
+        "title": title,
+        "description": json_data["description"],
+        "version": json_data["version"],
+        "properties": d,
+        "required": required
     }
-
-    for key, value in json_data.items():
-        if isinstance(value, str):
-            schema["properties"][key] = {
-                "type": "string",
-                "description": value
-            }
-        elif isinstance(value, list):
-            d = {}
-            required = []
-            for el in value:
-                if isinstance(el, dict):
-                    if el["field_type"] in ["TEXT_FIELD", "TEXT_AREA_FIELD"]:
-                        d[el["name"]] = {
-                            "type": "string",
-                            "description": el["description"]
-                        }
-                    elif el["field_type"] == "TEXT_CHOICE_FIELD":
-                        d[el["name"]] = {
-                            "type": "string",
-                            "description": el["description"],
-                            "enum": el["controlled_vocabulary"]                            
-                        }
-                    elif el["field_type"] == "DATE_FIELD":
-                        d[el["name"]] = {
-                            "type": "string",
-                            "description": el["description"],
-                            "format": "date"
-                        }
-                    if el["cardinality"] == "mandatory":
-                        required.append(el["name"])
-            print(d)
-            schema["properties"][key] = {
-                "type": "array",
-                "items": {
-                    "type": "object",
-                    "properties": d,
-                    "required": required
-                }
-            }
-        elif isinstance(value, dict):
-            schema["properties"][key] = generate_json_schema(value)
-        else:
-            schema["properties"][key] = {
-                "type": "string"
-            }
-
+    
     return schema
 
 def main():
@@ -61,7 +45,7 @@ def main():
         json_data = json.load(file)
 
     # Generate the JSON schema
-    json_schema = generate_json_schema(json_data["genomics_template"])
+    json_schema = generate_json_schema(json_data["genomics_template"], "Genomics template schema for single reads")
 
     # Save the JSON schema to a file
     with open('../genomics_template_schema_single_read.json', 'w') as file:
