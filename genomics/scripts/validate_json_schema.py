43298bcdb04f83a11f773253b71fd57299be374a
Sonja Mathias <sonja.mathias@scilifelab.uu.se>
2025-04-03 15:17:32 +0200

    Use iter_tools to catch all validation errors

--- genomics/scripts/validate_json_schema.py
+++ genomics/scripts/validate_json_schema.py
@@ -8,16 +8,22 @@ def load_json_schema(schema_file):
         return json.load(file)
 
 def load_tsv_data(tsv_file):
+    """
+    Load TSV data from a file and return it as a list of dictionaries.
+    Each dictionary represents a row in the TSV file, with keys as column headers."""
     with open(tsv_file, 'r') as file:
         reader = csv.DictReader(file, delimiter='\t')
         return list(reader)
 
-def validate_data(data, schema):
-    try:
-        validate(instance=data, schema=schema)
-        print("Validation successful!")
-    except jsonschema.exceptions.ValidationError as err:
-        print("Validation error:", err)
+def validate_data(tsv_data, schema):
+    # Validate each row in the TSV data against the schema
+    for i, row in enumerate(tsv_data):
+        errors = list(jsonschema.Draft7Validator(schema).iter_errors(row))
+        if errors:
+            for error in errors:
+                print(f"Validation error in row {i+1}:", error.message)
+        else:
+            print("Validation successful!")
 
 def main():
     # Load the JSON schema
@@ -26,9 +32,7 @@ def main():
     # Load the TSV data
     tsv_data = load_tsv_data('../example_data/example-genomics_technical_metadata_single_read.tsv')
 
-    # Validate each row in the TSV data against the schema
-    for row in tsv_data:
-        validate_data(row, schema)
+    validate_data(tsv_data, schema)
 
 if __name__ == "__main__":
     main()
